f824de4dc3ac200c4148f3e383f5e5e9abe3555b
Ben <b.m.church@hotmail.com>
2017-01-27 22:07:36 +1300

    #package.json

---
+++ controllers/routes.js
@@ -0,0 +1,17 @@
+
+module.exports = function(app) {
+
+    //test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+    app.get('/api', function(req, res) {
+
+            res.json({ message: 'test working external route file' });
+            res.end();
+        });
+
+
+
+
+
+
+
+};
